9ad280d33072933a72148c7573d25f793f072366
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-05-09 18:39:47 -0400

    Update to error summary and target attribute in about.pug

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
+  console.log('Uh-Oh! There seems to have been an error!')
   res.render('error');
 });
 
